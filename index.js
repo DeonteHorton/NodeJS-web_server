aba144f66698a8bb9c2f804c6ae6831411814a18
DeonteHorton <deontedeshaunhorton@gmail.com>
2023-12-21 23:50:14 -0600

    Corrected 404 page styling and added comments to index.js file

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-// import httpServer from 'http';
 const httpServer = require('http');
 const fs = require('fs');
 const path = require('path');
 
 const PORT = 3000;
 
+// creates the http server
 const server = httpServer.createServer();
 
+// listens to request made to the server
 server.on('request', (req, res) => {
+    // sets the response header content type to html
     res.writeHead(200, {'Content-Type': 'text/html'});
-    let path = req.url.replace('/', '');
-    console.log({path});
+    let path = req.url.replace('/', ''); //gets the path to the files from the url, the first character is a '/' and is removed from the string
 
-    // check if path contains a '/', if so check if the first part of the path is a directory
-    if(path.indexOf('assets') > -1 && (path.indexOf('.css') > -1)) {
-
+    if(path.indexOf('assets') > -1 && (path.indexOf('.css') > -1)) { // checks if the string 'assets' and '.css' is in the detected in the path
+        // changes the response header content type to css to send over the css files for the html pages
         res.writeHead(200, {'Content-Type': 'text/css'});
         res.write(fs.readFileSync(`${path}`, 'utf8'));
         res.end();
-    } else if(path.indexOf('functionality') > -1 && (path.indexOf('.js') > -1)) {
+    } else if(path.indexOf('functionality') > -1 && (path.indexOf('.js') > -1)) { // checks if the string 'functionality' and '.js' is in the detected in the path
 
+        // changes the response header content type to javascript to send over the javascript files for the html pages
         res.writeHead(200, {'Content-Type': 'text/javascript'});
         res.write(fs.readFileSync(`${path}`, 'utf8'));
         res.end();
-    } else if(path === '' || path === 'index') {
+    } else if(path === '' || path === 'index') { // checks if the path is empty or index
 
         res.write(fs.readFileSync(`index.html`, 'utf8'));
         res.end();
-    } else if(fs.existsSync(`${path}.html`)) {
+    } else if(fs.existsSync(`${path}.html`)) { // checks if the file in the directory matches the path from the url
 
         res.write(fs.readFileSync(`${path}.html`, 'utf8'));
         res.end();
-    } else if(fs.existsSync(`${path}/index.html`)) {
+    } else if(fs.existsSync(`${path}/index.html`)) { // checks if a nested file within a directory matches the path from the url
 
         res.write(fs.readFileSync(`${path}/index.html`, 'utf8'))
         res.end();
-    } else if(path.indexOf('/') > -1 && checkForPathExistence(path, path.split('/'))) {
+    } else if(path.indexOf('/') > -1 && checkForPathExistence(path.split('/'))) { // checks if the path is nested and if the nested path exists
 
         res.write(fs.readFileSync(`${path}.html`, 'utf8'));
         res.end();
-    } else {
+    } else { // if the path does not exist, send the 404 page
 
         res.write(fs.readFileSync('404.html'));
         res.end();
@@ -56,11 +57,17 @@ server.listen(PORT, () => {
 });
 
 
-const checkForPathExistence = (requestedPath, paths, currPath='') => {
+/**
+ * Checks for the existence of a nested file in a given directory structure.
+ *
+ * @param {string} requestedPath - The requested path .
+ * @param {string[]} paths - An array of strings that were splitted that would represent file or directory.
+ * @param {string} [currPath=''] - The current path being evaluated (used for recursive calls).
+ * @returns {boolean} - Returns true if the path exists, false otherwise.
+ */
+const checkForPathExistence = (paths, currPath='') => {
     let exist = false;
     let arrayCount = paths.length
-
-    console.log({requestedPath, paths, currPath, arrayCount});
 
     if (arrayCount === 0) {
         if (fs.existsSync(`${path}.html`) || fs.existsSync(`${path}/index.html`)) {
@@ -78,7 +85,7 @@ const checkForPathExistence = (requestedPath, paths, currPath='') => {
                 exist = true;
                 break;
             } else {
-                checkForPathExistence(requestedPath, paths, path);
+                checkForPathExistence(paths, path);
             }
 
         } else {
